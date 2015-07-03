230de9613a7c0b18bb1caedeac50c403bb770d08
John Reeme <john.reeme@soteradefense.com>
2015-07-03 08:04:13 -0500

    Making changes so app will run in strongloop docker container

--- server/server.js
+++ server/server.js
@@ -1,3 +1,16 @@
+var mongoAddr = '"' + process.env.MONGO_PORT_27017_TCP_ADDR + '"';
+var mongoPort = process.env.MONGO_PORT_27017_TCP_PORT;
+var mysqlAddr = '"' + process.env.MYSQL_PORT_3306_TCP_ADDR + '"';
+var mysqlPort = process.env.MYSQL_PORT_3306_TCP_PORT;
+var fs = require('fs');
+
+var fileContents = fs.readFileSync(__dirname + '/datasources_template.json', 'utf8');
+fileContents = fileContents.replace(/MONGO_HOST_ADDR/g, mongoAddr);
+fileContents = fileContents.replace(/MONGO_HOST_PORT/g, mongoPort);
+fileContents = fileContents.replace(/MYSQL_HOST_ADDR/g, mysqlAddr);
+fileContents = fileContents.replace(/MYSQL_HOST_PORT/g, mysqlPort);
+fs.writeFileSync(__dirname + '/datasources.json', fileContents);
+
 var loopback = require('loopback');
 var boot = require('loopback-boot');
 
